383cae9aabbdad58ae476745737e90565ccd5156
shubham29062002 <shubhama2002@gmail.com>
2022-05-29 02:08:55 +0530

    Added Code for Switches and Displayed Count on Display

--- Mini Project/src/main.cpp
+++ Mini Project/src/main.cpp
@@ -1,17 +1,23 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
-int Secs = 0;
+int numSwitches = 4;
+int Switches[] = {6, 7, 8, 9};
+int Counts[] = {0, 0, 0, 0};
 
 //                RS, En, D4, D5, D6, D7  R/W-Gnd
 LiquidCrystal Lcd(12, 11, 5, 4, 3, 2);
+
+void PollingAndPrint(void);
 
 void setup()
 {
   // put your setup code here, to run once:
 
-  // Initialize LED pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  for (int i = 0; i < numSwitches; i++)
+  {
+    pinMode(Switches[i], INPUT);
+  }
 
   Lcd.begin(16, 2);
 
@@ -24,10 +30,31 @@ void loop()
 {
   // put your main code here, to run repeatedly:
 
+  PollingAndPrint();
+  delay(500);
+}
+
+void PollingAndPrint(void)
+{
   Lcd.setCursor(0, 1);
-  Lcd.print(Secs);
-  digitalWrite(LED_BUILTIN, (Secs % 2));
-  Serial.println(Secs);
-  delay(1000);
-  Secs += 1;
+  for (int i = 0; i < numSwitches; i++)
+  {
+    int reading = digitalRead(Switches[i]);
+    Serial.printf("Switch = %d, reading = %d\n", Switches[i], reading);
+    if (reading)
+    {
+      Counts[i] += 1;
+      Lcd.print(Counts[i]);
+      delay(5000);
+    }
+    else
+    {
+      Lcd.print(Counts[i]);
+    }
+    Serial.println(Counts[i]);
+    if (!(i == (numSwitches - 1)))
+      Lcd.print(",");
+  }
+  Serial.println("--------------------------------------------");
+  Serial.println("--------------------------------------------");
 }
