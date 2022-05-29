e15044cae9e6d8005fd69adfd00e5fcffe1994cf
shubham29062002 <shubhama2002@gmail.com>
2022-05-29 12:25:52 +0530

    Added project for ESP8266

---
+++ Mini Project Wifi/src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+
+int pin1 = 2;
+int pin2 = 16;
+
+void setup()
+{
+  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+  pinMode(pin1, OUTPUT);
+  pinMode(pin2, OUTPUT);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(pin1, HIGH);
+  digitalWrite(pin2, HIGH);
+  delay(1000);
+  digitalWrite(pin1, LOW);
+  digitalWrite(pin2, LOW);
+  delay(1000);
+
+  Serial.print("Doing Something\n");
+}
